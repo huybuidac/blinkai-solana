f6bb8c7e13efe583818763cba4b80682512ee578
huy bui dac <huybuidac@gmail.com>
2024-09-25 15:22:00 +0700

    feat: add simple vault and test

--- programs/blink-solana/src/lib.rs
+++ programs/blink-solana/src/lib.rs
@@ -1,16 +1,306 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    token_interface::{ self, Mint, TokenAccount, TokenInterface, TransferChecked },
+    associated_token::AssociatedToken,
+};
+use std::mem::size_of;
 
 declare_id!("FsXSdTn8whuMgCk93P1yHDUBy3xzPtrknnWx3YgWJ2VK");
+
+pub const STATE: &str = "state";
+pub const POOL: &str = "pool";
+pub const USER_POOL: &str = "user_pool";
+pub const VAULT: &str = "vault";
+pub const FEE_VAULT: &str = "fee-vault";
+
+const _100_PERCENT: u64 = 10000;
+pub const SLUG_LENGTH: usize = 32;
 
 #[program]
 pub mod blink_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_state(ctx: Context<CreateState>) -> Result<()> {
+        let mut state = ctx.accounts.state.load_init()?;
+        state.authority = ctx.accounts.authority.key();
+        Ok(())
+    }
+
+    pub fn create_pool(
+        ctx: Context<CreatePool>,
+        slug: String,
+        user_accepted_amount: u64,
+        fee: u64
+    ) -> Result<()> {
+        require!(slug.chars().all(|c| c.is_ascii_alphanumeric() || c == '-'), AppErrorCode::InvalidSlug);
+        // require!(slug.len() <= SLUG_LENGTH, AppErrorCode::InvalidSlug); // no need to check
+        require!(user_accepted_amount > 0, AppErrorCode::InvalidAcceptedAmount);
+        require!(fee <= _100_PERCENT, AppErrorCode::InvalidFeePercent);
+
+        let mut pool = ctx.accounts.pool.load_init()?;
+
+        pool.authority = ctx.accounts.authority.key();
+        pool.mint = ctx.accounts.mint.key();
+        pool.slug = str_to_slug(&slug);
+
+        pool.user_accepted_amount = user_accepted_amount;
+        pool.fee_percent = fee;
+        pool.vault = ctx.accounts.vault.key();
+        pool.fee_vault = ctx.accounts.fee_vault.key();
+
+        Ok(())
+    }
+
+    pub fn deposit(_ctx: Context<Deposit>, _slug: String) -> Result<()> {
+        let mut pool = _ctx.accounts.pool.load_mut()?;
+        let mut user_pool = match _ctx.accounts.user_pool.load_mut() {
+            Ok(user_pool) => user_pool,
+            Err(_) => {
+                let mut user_pool = _ctx.accounts.user_pool.load_init()?;
+                user_pool.pool = _ctx.accounts.pool.key();
+                user_pool.authority = _ctx.accounts.authority.key();
+                user_pool
+            }
+        };
+
+        require_eq!(user_pool.amount, 0, AppErrorCode::Deposited);
+
+        token_interface::transfer_checked(
+            CpiContext::new(_ctx.accounts.token_program.to_account_info(), TransferChecked {
+                from: _ctx.accounts.user_vault.to_account_info(),
+                to: _ctx.accounts.vault.to_account_info(),
+                authority: _ctx.accounts.authority.to_account_info(),
+                mint: _ctx.accounts.mint.to_account_info(),
+            }),
+            pool.user_accepted_amount,
+            _ctx.accounts.mint.decimals
+        )?;
+
+        user_pool.amount = pool.user_accepted_amount;
+        pool.total_amount += pool.user_accepted_amount;
+
+        Ok(())
+    }
+
+    pub fn withdraw(_ctx: Context<Withdraw>, _slug: String) -> Result<()> {
+        let mut pool = _ctx.accounts.pool.load_mut()?;
+        let mut user_pool = _ctx.accounts.user_pool.load_mut()?;
+
+        require_gt!(user_pool.amount, 0, AppErrorCode::NotDeposited);
+
+        let fee: u64 = u128
+            ::from(user_pool.amount)
+            .checked_mul(pool.fee_percent as u128)
+            .unwrap()
+            .checked_div(_100_PERCENT as u128)
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let withdraw_amount: u64 = user_pool.amount - fee;
+
+        pool.total_amount -= user_pool.amount;
+        pool.fee_amount += fee;
+        user_pool.amount = 0;
+
+        drop(pool);
+
+        let seeds = &[POOL.as_bytes(), _slug.as_bytes(), &[_ctx.bumps.pool]];
+        let signer = &[&seeds[..]];
+
+        token_interface::transfer_checked(
+            CpiContext::new_with_signer(
+                _ctx.accounts.token_program.to_account_info(),
+                TransferChecked {
+                    from: _ctx.accounts.vault.to_account_info(),
+                    to: _ctx.accounts.user_vault.to_account_info(),
+                    authority: _ctx.accounts.pool.to_account_info(),
+                    mint: _ctx.accounts.mint.to_account_info(),
+                },
+                signer
+            ),
+            withdraw_amount,
+            _ctx.accounts.mint.decimals
+        )?;
+
+        token_interface::transfer_checked(
+            CpiContext::new_with_signer(
+                _ctx.accounts.token_program.to_account_info(),
+                TransferChecked {
+                    from: _ctx.accounts.vault.to_account_info(),
+                    to: _ctx.accounts.fee_vault.to_account_info(),
+                    authority: _ctx.accounts.pool.to_account_info(),
+                    mint: _ctx.accounts.mint.to_account_info(),
+                },
+                signer
+            ),
+            fee,
+            _ctx.accounts.mint.decimals
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateState<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        seeds = [STATE.as_bytes()],
+        bump,
+        payer = authority,
+        space = 8 + size_of::<StateAccount>()
+    )]
+    pub state: AccountLoader<'info, StateAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(slug: String)]
+pub struct CreatePool<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut, seeds = [STATE.as_bytes()], bump, has_one = authority)]
+    pub state: AccountLoader<'info, StateAccount>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [POOL.as_bytes(), slug.as_bytes()],
+        bump,
+        space = 8 + size_of::<PoolAccount>()
+    )]
+    pub pool: AccountLoader<'info, PoolAccount>,
+    #[account(
+        init,
+        seeds = [VAULT.as_bytes(), slug.as_bytes()],
+        bump,
+        payer = authority,
+        token::mint = mint,
+        token::authority = pool,
+    )]
+    pub vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        init,
+        seeds = [FEE_VAULT.as_bytes(), slug.as_bytes()],
+        bump,
+        payer = authority,
+        token::mint = mint,
+        token::authority = pool,
+    )]
+    pub fee_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(slug: String)]
+pub struct Deposit<'info> {
+    #[account(mut, seeds = [POOL.as_bytes(), slug.as_bytes()], bump, has_one = vault @AppErrorCode::InvalidVault, has_one = mint @AppErrorCode::InvalidMint)]
+    pub pool: AccountLoader<'info, PoolAccount>,
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+    #[account(mut)]
+    pub vault: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init_if_needed,
+        seeds = [USER_POOL.as_bytes(), slug.as_bytes(), authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + size_of::<UserPoolAccount>()
+    )]
+    pub user_pool: AccountLoader<'info, UserPoolAccount>,
+    #[account(mut, constraint = user_vault.owner == authority.key() @AppErrorCode::Unauthorized, constraint = user_vault.mint == mint.key() @AppErrorCode::InvalidMint)]
+    pub user_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(slug: String)]
+pub struct Withdraw<'info> {
+    #[account(mut, seeds = [POOL.as_bytes(), slug.as_bytes()], bump, has_one = vault @AppErrorCode::InvalidVault, has_one = mint @AppErrorCode::InvalidMint, has_one = fee_vault @AppErrorCode::InvalidFeeVault)]
+    pub pool: AccountLoader<'info, PoolAccount>,
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+    #[account(mut)]
+    pub vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(mut)]
+    pub fee_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = authority
+    )]
+    pub user_vault: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut, seeds = [USER_POOL.as_bytes(), slug.as_bytes(), authority.key().as_ref()], bump)]
+    pub user_pool: AccountLoader<'info, UserPoolAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account(zero_copy)]
+pub struct StateAccount {
+    pub authority: Pubkey,
+}
+
+#[account[zero_copy]]
+pub struct PoolAccount {
+    pub authority: Pubkey,
+    pub mint: Pubkey,
+    pub vault: Pubkey,
+    pub fee_vault: Pubkey,
+    pub slug: [u8; SLUG_LENGTH],
+    pub user_accepted_amount: u64, // fixed amount - Accept X amount of token despoit by any user
+    pub total_amount: u64,
+    pub fee_percent: u64, // decimal 2
+    pub fee_amount: u64,
+}
+
+#[account(zero_copy)]
+pub struct UserPoolAccount {
+    pub pool: Pubkey,
+    pub authority: Pubkey,
+    pub amount: u64,
+}
+
+#[error_code]
+pub enum AppErrorCode {
+    #[msg("Invalid slug")]
+    InvalidSlug,
+    #[msg("Invalid accepted amount")]
+    InvalidAcceptedAmount,
+    #[msg("Invalid fee percent")]
+    InvalidFeePercent,
+    #[msg("User already deposited")]
+    Deposited,
+    #[msg("User not deposited")]
+    NotDeposited,
+    #[msg("Invalid vault")]
+    InvalidVault,
+    #[msg("Invalid fee vault")]
+    InvalidFeeVault,
+    #[msg("Invalid mint")]
+    InvalidMint,
+    #[msg("Unauthorized")]
+    Unauthorized,
+}
+
+pub fn str_to_slug(slug: &str) -> [u8; SLUG_LENGTH] {
+    // check slug includes characters, numbers, and hyphens only
+    let src = slug.as_bytes();
+    let mut data = [0u8; SLUG_LENGTH];
+    data[..src.len()].copy_from_slice(src);
+    data
+}
--- tests/blink-solana.ts
+++ tests/blink-solana.ts
@@ -1,16 +1,316 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { BlinkSolana } from "../target/types/blink_solana";
+import * as anchor from '@coral-xyz/anchor';
+import { Program, AnchorProvider, web3 } from '@coral-xyz/anchor';
+import chaiAsPromised from 'chai-as-promised';
+import chai, { assert, expect } from 'chai';
 
-describe("blink-solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+import { BlinkSolana } from '../target/types/blink_solana';
+// import { createToken } from './tests/utils'
+import { BN } from 'bn.js';
+import {
+  createAssociatedTokenAccount,
+  createMint,
+  getAssociatedTokenAddress,
+  getAssociatedTokenAddressSync,
+  getMint,
+  getTokenMetadata,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+  transfer,
+} from '@solana/spl-token';
 
-  const program = anchor.workspace.BlinkSolana as Program<BlinkSolana>;
+chai.use(chaiAsPromised);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+const ZERO = new BN(0);
+const POOL = Buffer.from('pool');
+const USER_POOL = Buffer.from('user_pool');
+
+const provider = anchor.getProvider() as AnchorProvider;
+const connection = provider.connection;
+const blink: Program<BlinkSolana> = anchor.workspace.BlinkSolana as Program<BlinkSolana>;
+const blinkErrors = anchor.parseIdlErrors(blink.idl);
+
+const users = Array.from({ length: 2 }, (_, index) => {
+  const user = anchor.web3.Keypair.generate();
+  const wallet = new anchor.Wallet(user);
+  const payer = (wallet as any).payer;
+  const provider = new anchor.AnchorProvider(connection, wallet, {});
+
+  return {
+    user,
+    payer,
+    wallet,
+    provider,
+    publicKey: user.publicKey,
+  };
+});
+type IUser = (typeof users)[0]
+const user1 = users[0];
+const user2 = users[1];
+
+describe('blink-solana', () => {
+  before(async () => {
+    await blink.methods.createState().rpc();
+    (await Promise.all([user1, user2])).map(async (u) => {
+      const lastBlockhash = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({
+        blockhash: lastBlockhash.blockhash,
+        lastValidBlockHeight: lastBlockhash.lastValidBlockHeight,
+        signature: await connection.requestAirdrop(u.publicKey, 10000 * web3.LAMPORTS_PER_SOL),
+      });
+    });
+  });
+  it('Can not recreate state', async () => {
+    await expect(blink.methods.createState().rpc()).rejectedWith('already in use');
+  });
+  describe('pools', () => {
+    let mint: anchor.web3.PublicKey;
+    let accounts: {};
+    before(async () => {
+      mint = await createToken(provider);
+      accounts = {
+        mint: mint,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      };
+    });
+    it('Invalid slug', async () => {
+      await expect(blink.methods.createPool('test ', ZERO, ZERO).accounts(accounts).rpc()).rejectedWith('Invalid slug');
+      await expect(blink.methods.createPool('test@', ZERO, ZERO).accounts(accounts).rpc()).rejectedWith('Invalid slug');
+      await expect(blink.methods.createPool('t'.repeat(33), ZERO, ZERO).accounts(accounts).rpc()).rejectedWith();
+    });
+    it('Invalid accepted amount', async () => {
+      await expect(blink.methods.createPool('test', ZERO, ZERO).accounts(accounts).rpc()).rejectedWith(
+        'Invalid accepted amount'
+      );
+    });
+    it('Invalid fee percent', async () => {
+      await expect(blink.methods.createPool('test', new BN(1), new BN(10001)).accounts(accounts).rpc()).rejectedWith(
+        'Invalid fee percent'
+      );
+    });
+    it('Create Pool OK', async () => {
+      await blink.methods.createPool('test', new BN(100), new BN(200)).accounts(accounts).rpc();
+      const pool = await blink.account.poolAccount.fetch(getPoolAddr('test'));
+      expect(pool.userAcceptedAmount.toNumber()).to.eq(100);
+      expect(pool.feePercent.toNumber()).to.eq(200);
+    });
+  });
+  describe('Deposit', () => {
+    let mint: web3.PublicKey;
+    let slug = 'dep1';
+    before(async () => {
+      mint = await createToken(provider);
+      await blink.methods
+        .createPool(slug, new BN(100), new BN(100))
+        .accounts({
+          mint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .rpc();
+    });
+    it('Can not deposit by other vault', async () => {
+      await mintToken(provider, mint, user2.publicKey, 1000);
+      const tx = await blink.methods
+        .deposit(slug)
+        .accounts({
+          authority: user1.publicKey,
+          userVault: getAssociatedTokenAddressSync(mint, user2.publicKey),
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .transaction();
+      await expect(user1.provider.sendAndConfirm(tx)).rejectedWith('Unauthorized');
+    });
+    it('Require token for deposit', async () => {
+      await expect(deposit(slug, user1)).rejectedWith('AccountNotInitialized');
+
+      await mintToken(provider, mint, user1.publicKey, 99);
+      await expect(deposit(slug, user1)).rejectedWith('insufficient funds');
+    });
+    it('User1 Deposit OK', async () => {
+      await mintToken(provider, mint, user1.publicKey, 100);
+      await deposit(slug, user1);
+
+      const user1Acc = await getUserPoolData(slug, user1);
+      expect(user1Acc.amount.toNumber()).to.eq(100);
+
+      const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+      expect(pool.totalAmount.toNumber()).to.eq(100);
+    });
+    it('User1 can not deposit twice', async () => {
+      await expect(deposit(slug, user1)).rejectedWith('User already deposited');
+    });
+    it('User2 Deposit OK', async () => {
+      await mintToken(provider, mint, user2.publicKey, 100);
+      await deposit(slug, user2);
+
+      const user2Acc = await getUserPoolData(slug, user2);
+      expect(user2Acc.amount.toNumber()).to.eq(100);
+
+      const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+      expect(pool.totalAmount.toNumber()).to.eq(200);
+    });
+  });
+  describe('Withdraw', () => {
+    let mint: web3.PublicKey;
+    let slug = 'wit1';
+    before(async () => {
+      mint = await createToken(provider);
+      await blink.methods
+        .createPool(slug, new BN(100), new BN(100))
+        .accounts({
+          mint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .rpc();
+    });
+    it('Deposits', async () => {
+      await mintToken(provider, mint, user1.publicKey, 100);
+      await mintToken(provider, mint, user2.publicKey, 100);
+      await deposit(slug, user1);
+      await deposit(slug, user2);
+    });
+    it('User1 Withdraw OK', async () => {
+      await withdraw(slug, user1);
+
+      const user1Acc = await getUserPoolData(slug, user1);
+      expect(user1Acc.amount.toNumber()).to.eq(0);
+
+      const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+      expect(pool.totalAmount.toNumber()).to.eq(100);
+      expect(pool.feeAmount.toNumber()).to.eq(1);
+    });
+    it('User1 can not withdraw twice', async () => {
+      await expect(withdraw(slug, user1)).rejectedWith('User not deposited');
+    });
+    it('User2 Withdraw OK', async () => {
+      await withdraw(slug, user2);
+
+      const user2Acc = await getUserPoolData(slug, user2);
+      expect(user2Acc.amount.toNumber()).to.eq(0);
+
+      const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+      expect(pool.totalAmount.toNumber()).to.eq(0);
+      expect(pool.feeAmount.toNumber()).to.eq(2);
+    });
+    it('Deposit again and withdraw', async () => {
+      await mintToken(provider, mint, user1.publicKey, 100);
+      await deposit(slug, user1);
+      await withdraw(slug, user1);
+    });
   });
 });
+
+async function withdraw(slug: string, user: IUser, mint?: web3.PublicKey) {
+  if (!mint) {
+    const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+    mint = pool.mint;
+  }
+  const tx = await blink.methods.withdraw(slug).accounts({
+    authority: user.publicKey,
+    tokenProgram: TOKEN_PROGRAM_ID,
+  }).transaction();
+  await user.provider.sendAndConfirm(tx);
+}
+
+async function deposit(slug: string, user: IUser, mint?: web3.PublicKey) {
+  if (!mint) {
+    const pool = await blink.account.poolAccount.fetch(getPoolAddr(slug));
+    mint = pool.mint;
+  }
+  const tx = await blink.methods.deposit(slug).accounts({
+    authority: user.publicKey,
+    userVault: getAssociatedTokenAddressSync(mint, user.publicKey),
+    tokenProgram: TOKEN_PROGRAM_ID,
+  }).transaction();
+  await user.provider.sendAndConfirm(tx);
+}
+
+async function getUserPoolData(slug: string, user: IUser) {
+  return await blink.account.userPoolAccount.fetch(getUserAccAddr(slug, user));
+}
+
+function getPoolAddr(slug: string) {
+  return web3.PublicKey.findProgramAddressSync([POOL, Buffer.from(slug)], blink.programId)[0];
+}
+
+function getUserAccAddr(slug: string, user: web3.PublicKey | IUser) {
+  let publicKey = 'publicKey' in user ? user.publicKey : user;
+  return web3.PublicKey.findProgramAddressSync(
+    [USER_POOL, Buffer.from(slug), publicKey.toBuffer()],
+    blink.programId
+  )[0];
+}
+
+export async function createToken(provider, authority = undefined, decimals = 9) {
+  if (!authority) {
+    authority = provider.wallet.publicKey;
+  }
+  const mint = await createMint(provider.connection, provider.wallet.payer, authority, null, decimals);
+  return mint;
+}
+
+export async function mintToken(
+  provider: AnchorProvider,
+  mint: web3.PublicKey,
+  toWallet: web3.PublicKey,
+  amount: number | bigint,
+  programId: web3.PublicKey = TOKEN_PROGRAM_ID
+) {
+  const payer = (provider.wallet as any).payer;
+  const associatedTokenAddress = await getAssociatedTokenAddress(mint, toWallet, true, programId);
+
+  const otaAcc = await provider.connection.getAccountInfo(associatedTokenAddress);
+  if (!otaAcc) {
+    await createAssociatedTokenAccount(provider.connection, payer, mint, toWallet, undefined, programId);
+  }
+
+  await mintTo(
+    provider.connection,
+    payer,
+    mint,
+    associatedTokenAddress,
+    provider.wallet.publicKey,
+    amount,
+    [],
+    {},
+    programId
+  );
+}
+
+export async function mintTokenToAccount(
+  provider: AnchorProvider,
+  mint: web3.PublicKey,
+  toAccount: web3.PublicKey,
+  amount: number | bigint
+) {
+  const payer = (provider.wallet as any).payer;
+
+  await mintTo(provider.connection, payer, mint, toAccount, provider.wallet.publicKey, amount);
+}
+
+export async function transferTo(
+  provider: AnchorProvider,
+  mint: web3.PublicKey,
+  toWallet: web3.PublicKey,
+  amount: number | bigint
+) {
+  const payer = (provider.wallet as any).payer;
+  const fromAcc = provider.wallet.publicKey;
+
+  const fromAta = await getAssociatedTokenAddress(mint, fromAcc, true);
+  const toAta = await getAssociatedTokenAddress(mint, toWallet, true);
+
+  const otaAcc = await provider.connection.getAccountInfo(toAta);
+  if (!otaAcc) {
+    await createAssociatedTokenAccount(provider.connection, payer, mint, toWallet);
+  }
+  await transfer(provider.connection, payer, fromAta, toAta, fromAcc, amount);
+}
+
+// export function toBN(val: any, decimals: number = 9) {
+//   const decimalAmount = parseUnits(val.toString(), decimals)
+//   return new BN(decimalAmount.toString())
+// }
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
